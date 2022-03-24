af3d7731ed19c02c247867cf5519b4242464982c
Josiahmpokera <jmpokera@outlook.com>
2022-03-24 20:34:23 +0300

    New Pushing

---
+++ android/app/src/main/java/com/example/saiba_conference_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.saiba_conference_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:saiba_conference_app/signup.dart';
+
+import 'home/homeScreen.dart';
+
+void main(){
+  runApp(const MyApplication());
+}
+
+class MyApplication extends StatefulWidget {
+  const MyApplication({Key? key}) : super(key: key);
+
+  @override
+  State<MyApplication> createState() => _MyApplicationState();
+}
+
+class _MyApplicationState extends State<MyApplication> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+     // home: SignUp(),
+      home: HomeScreen(),
+    );
+  }
+}
